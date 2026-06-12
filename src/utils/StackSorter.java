536f1fe1c5c29aa581f213bc216468b8bde1ab05
Oliver Valdiviezo <olivervaldiviezo67@gmail.com>
2026-06-12 10:55:06 -0500

    ADD: Ejercicio 02 - Stack Sorter en Java

---
+++ src/utils/StackSorter.java
@@ -0,0 +1,26 @@
+package utils;
+
+import java.util.Stack;
+
+public class StackSorter {
+
+    public void sortStack(Stack<Integer> stack){
+
+        Stack<Integer> ordenar = new Stack<>();
+
+        while(!stack.isEmpty()){
+            int aux = stack.pop();
+
+            while(!ordenar.isEmpty() && ordenar.peek()>aux){
+                stack.push(ordenar.pop());
+            }
+            ordenar.push(aux);
+        }
+
+        while (!ordenar.isEmpty()) {
+            stack.push(ordenar.pop());
+        }
+    }
+
+    
+}
